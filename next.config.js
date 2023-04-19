be51ef293b5fb5b943379503e0ec69ee730680b3
levi <levi@studypug.com>
2023-04-19 01:16:15 -0700

    updated2

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
   // output: "standalone",
   // basePath:"/next_sp",
-  // distDir: 'build',
+  distDir: 'build',
   trailingSlash: true,
   reactStrictMode: true,
   images: {
